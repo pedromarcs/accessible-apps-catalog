65ccd18623f577a0821ec5c89bab3a975a705894
pedromarcs <pedrodiimacedo@gmail.com>
2025-11-14 14:11:19 -0300

    fix: Corrigida a exportacao padrao em configuracoes/page.tsx

---
+++ app/configuracoes/page.tsx
@@ -0,0 +1,10 @@
+// app/configuracoes/page.tsx
+
+export default function ConfiguracoesPage() {
+  return (
+    <div>
+      <h1>Página de Configurações</h1>
+      {/* Aqui virão as opções de usuário */}
+    </div>
+  );
+}
