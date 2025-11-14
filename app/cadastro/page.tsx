fc818038b55c51468f2c45ca340b1ea8cce5c1a2
pedromarcs <pedrodiimacedo@gmail.com>
2025-11-14 14:08:15 -0300

    fix: Corrigida a exportacao padrao em cadastro/page.tsx

---
+++ app/cadastro/page.tsx
@@ -0,0 +1,10 @@
+// app/cadastro/page.tsx
+
+export default function CadastroPage() {
+  return (
+    <div>
+      <h1>Página de Cadastro de Usuário</h1>
+      {/* Aqui virá o formulário de cadastro */}
+    </div>
+  );
+}
