b9dd3aa5a168cda3d5c8cd2af7884af9919d377e
caiovini100 <caiomqnvinicius100@gmail.com>
2026-05-16 15:08:02 -0300

    atualização na conexão com API

--- app/favoritos/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/favoritos/page.tsx
-export default function FavoritosPage() {
-  return (
-    <div>
-      <h1>Meus Aplicativos Favoritos</h1>
-      {/* O colega de equipe irá construir a lista de apps favoritados aqui. */}
-    </div>
-  );
-}
--- app/termos/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/termos/page.tsx
-export default function TermosPage() {
-  return (
-    <div>
-      <h1>Termos e Condições de Uso</h1>
-      {/* O colega de equipe irá adicionar o texto legal e políticas aqui. */}
-    </div>
-  );
-}
